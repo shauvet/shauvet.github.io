1325db04cf165520d85e79b76dd4ce2f91f58a51
shauvet <285137889@qq.com>
2016-04-17 23:09:34 +0800

    update

--- baidu-work/yxw_task_2_32/main.js
+++ baidu-work/yxw_task_2_32/main.js
@@ -1,35 +1,145 @@
 /*生成表单*/
 
 var formFactory = (function(){
-    var formObj = {
-        label: '名称',                    // 表单标签
-        type: 'input',                   // 表单类型
-//        validator: function () {...},    // 表单验证规
-        rules: '必填，长度为4-16个字符',    // 填写规则提示
-        empty: '名称不能为空',            //验证为空
-        success: '格式正确',              // 验证通过提示
-        fail: '请按照格式要求填写'               // 验证失败提示
-    };
+
     /*common tools*/
     function $(id){
         return document.getElementById(id);
     }
-    function addEvent(type,func){
-        if(addEventListener){
-            return addEventListener(type,func,false);
-        }else{
-            return attachEvent(type,func);
+    // 页面加载完成后
+     function readyEvent(fn) {
+        if (fn==null) {
+            fn=document;
+        }
+        var oldonload = window.onload;
+        if (typeof window.onload != 'function') {
+            window.onload = fn;
+        } else {
+            window.onload = function() {
+                oldonload();
+                fn();
+            };
         }
     }
+    // 视能力分别使用dom0||dom2||IE方式 来绑定事件
+    // 参数： 操作的元素,事件名称 ,事件处理程序
+     function addEvent(element, type, handler) {
+        if (element.addEventListener) {
+            //事件类型、需要执行的函数、是否捕捉
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + type, function() {
+                handler.call(element);
+            });
+        } else {
+            element['on' + type] = handler;
+        }
+    }
+    // 移除事件
+     function removeEvent(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.datachEvent) {
+            element.detachEvent('on' + type, handler);
+        } else {
+            element['on' + type] = null;
+        }
+    }
+
 
     /*检查方法*/
     var check = (function(){
         return {
-            nameCheck:function(){},
-            passCheck:function(){},
-            repassCheck:function(){},
-            emailCheck:function(){},
-            phoneCheck:function(){}
+            nameCheck:function(){
+                var val = name.value;
+                var re = /[\u4E00-\u9FA5\uF900-\uFA2D]/;
+                var len = val.length;
+                var tip = name.nextElementSibling;
+                if(re.test(val)){len = len*2}
+                if(len < 4 || len > 16){
+                    tip.innerHTML = "必填，长度为4~16个字符";
+                    tip.style.color = "#f00";
+                    name.style.borderColor = "#f00";
+                }
+                else{
+                    tip.innerHTML = "名称格式正确";
+                    tip.style.color = "#0a0";
+                    name.style.borderColor = "#0a0";
+                    return true;
+                }
+                return false;
+            },
+            passCheck:function(){
+                var val = pass.value;
+                var re = /^[0-9a-zA-Z]*$/;
+                var len = val.length;
+                var tip = pass.nextElementSibling;
+                if(!re.test(val)){
+                    tip.innerHTML = "请输入字母或数字";
+                    tip.style.color = "#f00";
+                    pass.style.borderColor = "#f00";
+                }else if(len <4 || len > 16){
+                    tip.innerHTML = "必填，长度为4~16个字符";
+                    tip.style.color = "#f00";
+                    pass.style.borderColor = "#f00";
+                }
+                else{
+                    tip.innerHTML = "密码可用";
+                    tip.style.color = "#0a0";
+                    pass.style.borderColor = "#0a0";
+                    return true;
+                }
+                return false;
+            },
+            confirm:function(){
+                var val1 = pass.value;
+                var val2 = repass.value;
+                var tip = repass.nextElementSibling;
+                if(val1 == val2){
+                    tip.innerHTML = "密码输入一致";
+                    tip.style.color = "#0a0";
+                    repass.style.borderColor = "#0a0";
+                    return true;
+                }
+                else{
+                    tip.innerHTML = "两次输入密码不一致";
+                    tip.style.color = "#f00";
+                    repass.style.borderColor = "#f00";
+                }
+                return false;
+            },
+            emailCheck:function(){
+                var val = email.value;
+                var re = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
+                var tip = email.nextElementSibling;
+                if(!re.test(val)){
+                    tip.innerHTML = "邮箱格式错误";
+                    tip.style.color = "#f00";
+                    email.style.borderColor = "#f00";
+                    return false;
+                }else{
+                    tip.innerHTML = "邮箱格式正确";
+                    tip.style.color = "#0a0";
+                    email.style.borderColor = "#0a0";
+                    return true;
+                }
+            },
+            phoneCheck:function(){
+                var val = phone.value;
+                var re = /^1[3|4|5|7|8]\d{9}$/;
+                var tip = phone.nextElementSibling;
+                if(!re.test(val)){
+                    tip.innerHTML = "手机格式错误";
+                    tip.style.color = "#f00";
+                    phone.style.borderColor = "#f00";
+                    return false;
+                }else{
+                    tip.innerHTML = "手机格式正确";
+                    tip.style.color = "#0a0";
+                    phone.style.borderColor = "#0a0";
+                    return true;
+                }
+            }
         };
 
     })();
@@ -50,10 +160,11 @@ var formFactory = (function(){
 
     }
 
-    var name = new FormList('名称','input','必填，长度为4-16个字符',nameCheck);
-    var password = new FormList('密码','password','必填，长度为6-8个字符',passCheck);
-    var rePassword = new FormList('确认密码','password','与密码相同',repassCheck);
-    var email = new FormList('邮箱','email','请输入邮箱',emailCheck);
-    var phone = new FormList('手机','telephone','请输入手机号',phoneCheck);
+    /*实例化*/
+    var name = new FormList('名称','input','必填，长度为4-16个字符',check.nameCheck);
+    var password = new FormList('密码','password','必填，长度为6-8个字符',check.passCheck);
+    var confirm = new FormList('确认密码','password','与密码相同',check.confirm);
+    var email = new FormList('邮箱','email','请输入邮箱',check.emailCheck);
+    var phone = new FormList('手机','telephone','请输入手机号',check.phoneCheck);
 
 })();
