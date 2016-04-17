f321e935982ad487ec3c7306899c9fe81d1a102c
shauvet <285137889@qq.com>
2016-04-17 11:19:09 +0800

    update task32

--- baidu-work/yxw_task_2_32/main.js
+++ baidu-work/yxw_task_2_32/main.js
@@ -1,23 +1,40 @@
-/*自动生成表单*/
+/*生成表单*/
 
 var formFactory = (function(){
-
     var formObj = {
         label: '名称',                    // 表单标签
         type: 'input',                   // 表单类型
-        validator: function () {...},    // 表单验证规
+//        validator: function () {...},    // 表单验证规
         rules: '必填，长度为4-16个字符',    // 填写规则提示
         empty: '名称不能为空',            //验证为空
         success: '格式正确',              // 验证通过提示
         fail: '请按照格式要求填写'               // 验证失败提示
     };
-    var labelObj = document.createElement('label');
-    var labelText = document.createTextNode(formObj.label);
-    labelObj.appendChild(labelText);
-    var typeObj = document.createElement(formObj.type);
+    /*common tools*/
+    function $(id){
+        return document.getElementById(id);
+    }
+    function addEvent(type,func){
+        if(addEventListener){
+            return addEventListener(type,func,false);
+        }else{
+            return attachEvent(type,func);
+        }
+    }
 
-    var submit = document.createElement('input');
-    submit.setAttribute('type','submit');
-    submit.setAttribute('value','提交')
+    var nameCre = $('name');
+    var passwordCre = $('password');
+    var emailCre = $('email');
+    var phoneCre = $('phone');
+
+    var formCre = $('formCreate')
+
+    function FormList(label,type,rules,validator){
+        this.label = label;
+        this.type = type;
+        this.rules = rules;
+        this.validator = validator;
+
+    }
 
 })();
