c3902aea0ccb6cfb32a47d3ff8e33936295ebb5e
shauvet <285137889@qq.com>
2016-04-26 23:13:24 +0800

    update

--- baidu-work/yxw_task_3_38/main.js
+++ baidu-work/yxw_task_3_38/main.js
@@ -6,22 +6,24 @@ var sortTable = (function(){
     var newTable = document.createElement('table');
     var newThead = document.createElement('thead');
     var newTbody = document.createElement('tbody');
-    var newTfoot = document.createElement('tfoot');
 
+    var title = ['姓名','语文','数学','英语','总分'];
     var data = {
-        '姓名':['小明','小红','小亮'],
-        '语文':['80','90','60'],
-        '数学':['90','60','100'],
-        '英语':['70','90','70'],
-        '总分':['240','240','230']
-    };
-    var course,
-        strHead = '<tr>',
-        strBody = '<tr>',
-        i;
-    for(course in data){
-        strHead += '<th>' + course + '</th>';
-
+        '小明':['80','90','70','240'],
+        '小红':['90','60','90','240'],
+        '小亮':['60','100','70','230']
+};
+    var strHead = '<tr>',
+        strBody = '',
+        i,j;
+    for(i=0; i<title.length; i++){
+        strHead += '<th>' + title[i] + '</th>';
+    }
+    for(key in data){
+        strBody += '<tr><td>' + key + '</td>';
+        for(j=0; j<data[key].length; j++){
+            strBody += '<td>' + data[key][j] + '</td>';
+        }
     }
     strHead += '</tr>';
     strBody += '</tr>';
@@ -30,4 +32,48 @@ var sortTable = (function(){
     newTable.appendChild(newThead);
     newTable.appendChild(newTbody);
     tableWrapper.appendChild(newTable);
+
+
+    function SortTable(tableEle, thead, data, funcSort){
+        this.tableEle = tableEle;
+        this.thead = thead;
+        this.data = data;
+        this.funcSort = funcSort;
+        this.curOrder = null;
+        this.init();
+    }
+
+    SortTable.prototype = {
+        init: function(){
+            this.curOrder = [];
+            for (var key in this.data){
+                this.curOrder.push(key);
+            }
+            this.render();
+        },
+        render: function(){
+            var strHead = '<tr>',
+                strBody = '',
+                i,j;
+            for(i=0; i<this.thead.length; i++){
+                strHead += '<th>' + this.thead[i] + '</th>';
+            }
+            for(key in this.data){
+                strBody += '<tr><td>' + key + '</td>';
+                for(j=0; j<this.data[key].length; j++){
+                    strBody += '<td>' + this.data[key][j] + '</td>';
+                }
+            }
+            strHead += '</tr>';
+            strBody += '</tr>';
+
+            this.tableEle += strHead;
+            this.tableEle += strBody;
+
+            this.addSortEle();
+        },
+        addSortEle: function(){
+
+        }
+    }
 })();
